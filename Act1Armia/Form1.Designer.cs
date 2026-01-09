7f8df2987167910a1f1eec0e4eff40e3e8e2da58
Armia <Armia@DESKTOP-EJ2JS9N>
2026-01-10 00:21:54 +0800

    Add project files.

---
+++ Act1Armia/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Act1Armia
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.Cnfrmpasstxt = new System.Windows.Forms.TextBox();
+            this.Yeartxt = new System.Windows.Forms.TextBox();
+            this.Passtxt = new System.Windows.Forms.TextBox();
+            this.Invitetxt = new System.Windows.Forms.TextBox();
+            this.Grouptxt = new System.Windows.Forms.TextBox();
+            this.ProcessBtn = new System.Windows.Forms.Button();
+            this.Dragpnl = new System.Windows.Forms.Panel();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.invitelbl = new System.Windows.Forms.Label();
+            this.Agelbl = new System.Windows.Forms.Label();
+            this.Cnfrmlbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.Dragpnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 10;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.Location = new System.Drawing.Point(6, 61);
+            this.Nametxt.Multiline = true;
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(196, 23);
+            this.Nametxt.TabIndex = 1;
+            // 
+            // Cnfrmpasstxt
+            // 
+            this.Cnfrmpasstxt.Location = new System.Drawing.Point(6, 193);
+            this.Cnfrmpasstxt.Multiline = true;
+            this.Cnfrmpasstxt.Name = "Cnfrmpasstxt";
+            this.Cnfrmpasstxt.Size = new System.Drawing.Size(196, 23);
+            this.Cnfrmpasstxt.TabIndex = 2;
+            // 
+            // Yeartxt
+            // 
+            this.Yeartxt.Location = new System.Drawing.Point(6, 105);
+            this.Yeartxt.Multiline = true;
+            this.Yeartxt.Name = "Yeartxt";
+            this.Yeartxt.Size = new System.Drawing.Size(196, 23);
+            this.Yeartxt.TabIndex = 3;
+            // 
+            // Passtxt
+            // 
+            this.Passtxt.Location = new System.Drawing.Point(6, 149);
+            this.Passtxt.Multiline = true;
+            this.Passtxt.Name = "Passtxt";
+            this.Passtxt.Size = new System.Drawing.Size(196, 23);
+            this.Passtxt.TabIndex = 4;
+            // 
+            // Invitetxt
+            // 
+            this.Invitetxt.Location = new System.Drawing.Point(6, 281);
+            this.Invitetxt.Multiline = true;
+            this.Invitetxt.Name = "Invitetxt";
+            this.Invitetxt.Size = new System.Drawing.Size(196, 23);
+            this.Invitetxt.TabIndex = 5;
+            // 
+            // Grouptxt
+            // 
+            this.Grouptxt.Location = new System.Drawing.Point(6, 237);
+            this.Grouptxt.Multiline = true;
+            this.Grouptxt.Name = "Grouptxt";
+            this.Grouptxt.Size = new System.Drawing.Size(196, 23);
+            this.Grouptxt.TabIndex = 6;
+            // 
+            // ProcessBtn
+            // 
+            this.ProcessBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessBtn.Location = new System.Drawing.Point(48, 310);
+            this.ProcessBtn.Name = "ProcessBtn";
+            this.ProcessBtn.Size = new System.Drawing.Size(99, 34);
+            this.ProcessBtn.TabIndex = 7;
+            this.ProcessBtn.Text = "Process";
+            this.ProcessBtn.UseVisualStyleBackColor = true;
+            this.ProcessBtn.Click += new System.EventHandler(this.ProcessBtn_Click);
+            // 
+            // Dragpnl
+            // 
+            this.Dragpnl.Controls.Add(this.ExitBtn);
+            this.Dragpnl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Dragpnl.Location = new System.Drawing.Point(0, 0);
+            this.Dragpnl.Name = "Dragpnl";
+            this.Dragpnl.Size = new System.Drawing.Size(328, 35);
+            this.Dragpnl.TabIndex = 8;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.FlatAppearance.BorderSize = 0;
+            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitBtn.Location = new System.Drawing.Point(287, 0);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(44, 35);
+            this.ExitBtn.TabIndex = 0;
+            this.ExitBtn.Text = "✖";
+            this.ExitBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.Dragpnl;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // invitelbl
+            // 
+            this.invitelbl.AutoSize = true;
+            this.invitelbl.Location = new System.Drawing.Point(202, 286);
+            this.invitelbl.Name = "invitelbl";
+            this.invitelbl.Size = new System.Drawing.Size(35, 13);
+            this.invitelbl.TabIndex = 9;
+            this.invitelbl.Text = "label1";
+            this.invitelbl.Visible = false;
+            // 
+            // Agelbl
+            // 
+            this.Agelbl.AutoSize = true;
+            this.Agelbl.Location = new System.Drawing.Point(202, 110);
+            this.Agelbl.Name = "Agelbl";
+            this.Agelbl.Size = new System.Drawing.Size(35, 13);
+            this.Agelbl.TabIndex = 10;
+            this.Agelbl.Text = "label1";
+            this.Agelbl.Visible = false;
+            // 
+            // Cnfrmlbl
+            // 
+            this.Cnfrmlbl.AutoSize = true;
+            this.Cnfrmlbl.Location = new System.Drawing.Point(202, 198);
+            this.Cnfrmlbl.Name = "Cnfrmlbl";
+            this.Cnfrmlbl.Size = new System.Drawing.Size(35, 13);
+            this.Cnfrmlbl.TabIndex = 11;
+            this.Cnfrmlbl.Text = "label1";
+            this.Cnfrmlbl.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Full Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Birth Year";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Password";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Confirm Password";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Group Code";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Invite Code";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 353);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Cnfrmlbl);
+            this.Controls.Add(this.Agelbl);
+            this.Controls.Add(this.invitelbl);
+            this.Controls.Add(this.Dragpnl);
+            this.Controls.Add(this.ProcessBtn);
+            this.Controls.Add(this.Grouptxt);
+            this.Controls.Add(this.Invitetxt);
+            this.Controls.Add(this.Passtxt);
+            this.Controls.Add(this.Yeartxt);
+            this.Controls.Add(this.Cnfrmpasstxt);
+            this.Controls.Add(this.Nametxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RegistrationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Dragpnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private System.Windows.Forms.Button ProcessBtn;
+        private System.Windows.Forms.TextBox Grouptxt;
+        private System.Windows.Forms.TextBox Invitetxt;
+        private System.Windows.Forms.TextBox Passtxt;
+        private System.Windows.Forms.TextBox Yeartxt;
+        private System.Windows.Forms.TextBox Cnfrmpasstxt;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.Panel Dragpnl;
+        private System.Windows.Forms.Button ExitBtn;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private System.Windows.Forms.Label Cnfrmlbl;
+        private System.Windows.Forms.Label Agelbl;
+        private System.Windows.Forms.Label invitelbl;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+    }
+}
+
---
+++ Act1Armia/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act1Armia
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessBtn_Click(object sender, EventArgs e)
+        {
+            Year();
+            Password();
+            Code();
+        }
+        public void Year()
+        {
+            Agelbl.Visible = true;
+            int year; 
+            year = Convert.ToInt32(Yeartxt.Text);
+            if (year <= 1900 || year >= 2025)
+            {
+                Agelbl.Text = "Year is out of range";
+            }
+            else
+            { int age = 2024 - year;
+                Agelbl.Text = "Your Age is: " + age.ToString();
+            }
+            
+
+        }
+
+        public void Password()
+        {
+            Cnfrmlbl.Visible= true;
+            if (Passtxt.Text == Cnfrmpasstxt.Text)
+            {
+                Cnfrmlbl.Text = "Accepted";
+
+            }
+            else
+            {
+                Cnfrmlbl.Text = "Password does not match";
+            }
+        }
+        public void Code()
+        {
+            invitelbl.Visible = true;
+            if (Grouptxt.Text == "UCC2025" && Invitetxt.Text == "PROMO1225" || Invitetxt.Text == "PROMO2025")
+            {
+                invitelbl.Text = "Accepted";
+            }
+            else
+            {
+                invitelbl.Text = "Code does not match";
+            }
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
